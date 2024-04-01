2a70ea2fdc383ff7b5ecc4406c23992fddfee24a
AdrianPricopie <adi.pricopie@yahoo.com>
2024-04-01 15:04:58 +0300

    update

--- cypress/fixtures/Integration/Tests/BDD/SearchFeature/Search.feature
+++ cypress/fixtures/Integration/Tests/BDD/SearchFeature/Search.feature
@@ -4,13 +4,23 @@ Feature:Search Functionality
      Given I am logged in on the page 
 
  Scenario Outline: Search multiple products
-    When I enter "<keyword>" in the search bar
+    When I introduce "<ValidProduct>" in the search bar
     And I click the search button
-    Then I should see search results for "<keyword>"
+    Then I should see search results for "<ValidProduct>"
 
     Examples:
-      | keyword        |
+      | ValidProduct    |
       | iPhone         |
       | MacBook        |
       | SmartWatch   |
 
+  Scenario Outline: Search price range product
+  When I enter "<product>" in the search bar
+  And I click the search button
+  And I click "<keyword>" price filter
+  Then I should see the prices for items in "<keyword>" order
+
+Examples:
+  | product       | keyword           |
+  | SmartWatch    | Pret crescator     |
+  | Phone         | Pret descrescator  |
--- cypress/fixtures/Integration/Tests/BDD/SearchFeature/Search.js
+++ cypress/fixtures/Integration/Tests/BDD/SearchFeature/Search.js
@@ -3,16 +3,17 @@ import {When, Then ,Given, } from "@badeball/cypress-cucumber-preprocessor";
 Given("I am logged in on the page", () => {
     cy.loginToPage("BogdanTest@yahoo.com","Test123")
     cy.get("#cookie-notification__accept").click()
+    
    });
 
-When("I enter {string} in the search bar", (keyword) => {
-    cy.get('.sn-suggest-input').type(keyword)
+When("I introduce {string} in the search bar", (ValidProduct) => {
+    cy.get('.sn-suggest-input').type(ValidProduct)
 });
 When("I click the search button",() => {
 cy.get('input[type="submit"]').click()
 });
-Then("I should see search results for {string}",(keyword)=>{
-    const keywordLowerCase = keyword.toLowerCase(); // Convertim cuvântul cheie în litere mici
+Then("I should see search results for {string}",(ValidProduct)=>{
+    const keywordLowerCase = ValidProduct.toLowerCase(); // Convertim cuvântul cheie în litere mici
 
     cy.get(".npi_name").each(($element) => {
       const elementText = $element.text().toLowerCase(); // Convertim textul din element în litere mici
@@ -20,3 +21,49 @@ Then("I should see search results for {string}",(keyword)=>{
       expect(elementText).to.contain(keywordLowerCase); // Verificăm dacă textul elementului conține cuvântul cheie în litere mici
     });
   });
+Then("I should see the prices for itmes  in ascending order",()=>{
+  cy.wait(2000)
+  const prices = []
+  cy.get('.real_price').each(($element) => {
+   const TitlePrice =  $element.text()
+   const FormatText = parseFloat(TitlePrice.trim().slice(0,-4).replace(",","."))
+    prices.push(FormatText)
+
+}).then(()=>{
+  for (let i = 0; i < prices.length - 1; i++) {
+    expect(prices[i]).to.be.at.most(prices[i + 1]);
+  }
+  console.log(prices)
+  });
+});
+
+When("I enter {string} in the search bar",(product)=>{
+  cy.get('.sn-suggest-input').type(product)
+})
+
+When("I click {string} price filter",(keyword)=>{
+  cy.get('#sortWidget').select(keyword)
+})
+
+Then("I should see the prices for items in {string} order",(keyword)=>
+{
+  cy.wait(2000)
+  const prices = []
+  cy.get('.real_price').each(($element) => {
+   const TitlePrice =  $element.text()
+   const FormatText = parseFloat(TitlePrice.trim().slice(0,-4).replace(",","."))
+    prices.push(FormatText)
+
+}).then(()=>{
+  if(keyword === 'Pret crescator'){
+  for (let i = 0; i < prices.length - 1; i++) {
+    expect(prices[i]).to.be.at.most(prices[i + 1]);
+  }
+  }
+  if(keyword === 'Pret descrescator'){
+    for (let i = 0; i < prices.length - 1; i++) {
+      expect(prices[i]).to.be.at.least(prices[i + 1]);
+    }
+  }
+  });
+});
